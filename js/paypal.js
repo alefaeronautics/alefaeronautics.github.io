ea438a6bff962554ceafca0531d53ca879ef34cd
alefaeronautics <111001947+alefaeronautics@users.noreply.github.com>
2023-07-06 07:43:15 +0545

    checking removed
    Additional layer of checking the amount has been removed

--- js/paypal.js
+++ js/paypal.js
@@ -56,9 +56,9 @@ function initPayPalButton() {
       },
 
     createOrder: function(data, actions) {    
-        var value = parseInt($("#contact-advance").attr('value')); 
-        value = checkAmount(value);
-        var description = "Alef Flying Car pre-order.\nRefundable deposit for your "+ ( (value>150) ? "priority" : "general" ) +" queue place in line.";
+        var value = $("#contact-advance").attr('value'); 
+        //value = checkAmount(value);
+        var description = "Alef Flying Car pre-order.\nRefundable deposit for your "+ ( (value=='1500') ? "priority" : "general" ) +" queue place in line.";
         var order_data = {
             "description": description,
             "amount":{
