473eb69ef3c73687a497c663172d3a30ff1983d2
aejiou <khandro.an@gmail.com>
2023-12-11 20:54:19 +0700

    refer call test

--- js/refer.js
+++ js/refer.js
@@ -24,6 +24,37 @@ const discount = {
     }
 }
 
+const discount1 = {
+    code: "",
+    getPrice: async function() {
+        if (this.code=='') return 150;
+
+        const oceanRefer = await $.ajax({
+            url: "https://deep-water-rucks.ondigitalocean.app/referral/",
+            type: "POST",
+            data: {
+              code: this.code
+            }
+          });
+          var result = await oceanRefer.value;
+          return parseInt(result);
+    },
+    getCode: function() {
+        var url = window.location.href;
+        if (url.lastIndexOf('#')!=-1) this.code = url.substring(url.lastIndexOf('#')+1,url.length).split('?')[0];
+        else {
+            var user_ref = new URLSearchParams(window.location.search).get("user_referral");
+            this.code = (user_ref) ? user_ref : "";
+        }
+        return this.code;
+    },
+}
+
+var referral1_code = discount1.getCode();
+var preorder1_price = await discount1.getPrice();
+
+
 var referral_code = discount.getCode();
 const preorder_price = discount.getPrice();
 const referral_number = 7;
+
